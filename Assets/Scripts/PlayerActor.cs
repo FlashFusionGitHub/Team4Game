7a55458993afd0d62c2b659e9ad397a74f774323
Rowen <xeno.spam1993@gmail.com>
2018-04-23 14:37:24 +0930

    Player Controller.

---
+++ Assets/Scripts/PlayerActor.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerActor : MonoBehaviour {
+
+	private CharacterController player;
+	private float playerSpeed;
+
+
+	[SerializeField]private float walkSpeed;
+	[SerializeField]private float sprintSpeed;
+	[SerializeField]private float turningSpeed;
+	[SerializeField]private float gravity;
+
+	// Use this for initialization
+	void Start () {
+		player = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Apply gravity
+		Gravity ();
+
+		//Player Controls
+		PlayerTurn();
+		PlayerMovement ();
+		PlayerSprint ();
+	}
+
+	void Gravity() {
+		player.Move (new Vector3(0, -(gravity * Time.deltaTime), 0));
+	}
+
+	void PlayerMovement() {
+		if (Input.GetKey (KeyCode.W)) {
+			transform.Translate (Vector3.forward * playerSpeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate (-(Vector3.right * playerSpeed * Time.deltaTime));
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate (-(Vector3.forward * playerSpeed * Time.deltaTime));
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate (Vector3.right * playerSpeed * Time.deltaTime);
+		}
+	}
+
+	void PlayerTurn() {
+		transform.Rotate (Vector3.up, turningSpeed * Input.GetAxis("Mouse X"));
+	}
+
+	void PlayerSprint() {
+		if (Input.GetKey (KeyCode.LeftShift)) {
+			playerSpeed = sprintSpeed;
+		} else {
+			playerSpeed = walkSpeed;
+		}
+	}
+}
---
+++ Assets/Scripts/WeaponActor.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum FireMode {
+	Blue = 0,
+	Green,
+	Red
+};
+
+public class WeaponActor : MonoBehaviour {
+
+	[SerializeField]private GameObject laserSpawnPoint;
+	[SerializeField]private int range;
+
+	private FireMode firemode;
+	private LineRenderer laser;
+
+	// Use this for initialization
+	void Start () {
+		firemode = FireMode.Blue;
+		laser = gameObject.AddComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		SwitchFireMode ();
+
+		if (Input.GetMouseButtonDown (0)) {
+			ShootLaser ();
+		}
+	}
+
+	void ShootLaser() {
+		RaycastHit hit;
+
+		Physics.Raycast (laserSpawnPoint.transform.position, laserSpawnPoint.transform.forward, out hit, range);
+
+		if (firemode == FireMode.Blue) {
+
+			laser.startColor = Color.blue;
+
+			if (hit.collider.tag == "Blue Enemy") {
+				Debug.Log ("Killed Blue Enemy : " + hit.collider);
+			} else {
+				Debug.Log ("Blue Laser doesn't effect : " + hit.collider);
+			}
+		}
+		
+		if (firemode == FireMode.Green) {
+		
+			if (hit.collider.tag == "Green Enemy") {
+				Debug.Log ("Killed Green Enemy : " + hit.collider);
+			} else {
+				Debug.Log ("Green Laser doesn't effect : " + hit.collider);
+			}
+		}
+		
+		if (firemode == FireMode.Red) {
+		
+			if (hit.collider.tag == "Red Enemy") {
+				Debug.Log ("Killed Red Enemy : " + hit.collider);
+			} else {
+				Debug.Log ("Red Laser doesn't effect : " + hit.collider);
+			}
+		}
+	}
+
+	void SetUpLaser() {
+		laser.enabled = true;
+		laser.SetPosition (0, laserSpawnPoint.transform.position);
+		laser.SetPosition (1, Vector3.forward * range);
+	}
+
+	void SwitchFireMode() {
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			firemode = FireMode.Blue;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha2)) {
+			firemode = FireMode.Green;
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha3)) {
+			firemode = FireMode.Red;
+		}
+	}
+}
