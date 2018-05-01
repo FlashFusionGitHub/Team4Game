6458033c120326fbc96b3ddc164fc392ff43e665
Rowen <rowen.govender93@gmail.com>
2018-05-01 20:53:58 +0930

    Win box added.

---
+++ Assets/Scripts/WinScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class WinScene : MonoBehaviour {
+
+    public Button restartButton;
+    public Button exitButton;
+
+    // Use this for initialization
+    void Start () {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        restartButton.onClick.AddListener(Restart);
+        exitButton.onClick.AddListener(Exit);
+    }
+
+    void Exit()
+    {
+        Application.Quit();
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
